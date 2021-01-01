010f8ea71f29355197cde71c1f696319e6386f2c
sleonia <nikita.dontsov@yahoo.com>
2021-01-02 06:09:21 +1000

    Initial

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App: FunctionComponent = () => {
+	return (<>Wow, it's magic</>);
+};
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { UnusedFilesWebpackPlugin } = require("unused-files-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.tsx',
+    target: 'web',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, './public'),
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+        alias: {
+            Libui: path.resolve('./src/libui/'),
+            Public: path.resolve('./public/'),
+            Styles: path.resolve('./src/styles/'),
+            Constants: path.resolve('./src/Constants/'),
+        }
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                loader: 'awesome-typescript-loader',
+                exclude: /node_modules/
+            },
+            {
+              test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+              loader: 'file-loader',
+              options: {
+                name: '[path][name].[ext]'
+              }
+            },
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: 'source-map-loader',
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader',
+            },
+        ],
+    },
+    plugins: [
+        new UnusedFilesWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        }),
+        new MiniCssExtractPlugin({
+            filename: './src/components/index.css',
+        }),
+    ],
+};
