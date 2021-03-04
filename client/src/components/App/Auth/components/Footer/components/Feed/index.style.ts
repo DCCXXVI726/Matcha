a5f3f32d85e33273b8f836668f53be9e3a8020de
Донцов Никита Андреевич <out-dontsov-na@cab-wsm-0006651.sigma.sbrf.ru>
2021-03-04 19:23:32 +0300

    Starting from zero

--- client/src/components/App/Auth/components/Footer/components/Feed/index.style.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const FeedStyled = styled.div`
-    display: flex;
-    overflow-x: hidden;
-
-`;
--- client/src/components/App/Auth/components/Footer/index.style.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const FooterStyled = styled.div`
-    margin: 30px auto;
-    width: 70%;
-    user-select: none;
-
-`;
