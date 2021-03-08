2f484b5fd783cd7055d3d94a9ecf42c3f164ce5c
sleonia <nikita.dontsov@yahoo.com>
2021-03-08 14:04:03 +0300

    Deleted useless readmes and make nodejs like middle

--- client/stub/index.ts
+++ client/stub/index.ts
@@ -1,43 +1,26 @@
-// import path = require('path');
-// import express = require('express');
-// import { Response } from 'express';
-// import webpack = require('webpack');
-// import webpackDevMiddleware = require('webpack-dev-middleware');
-// /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
-// /* @ts-ignore */
-// import webpackConfig = require('../webpack-config');
+import path = require('path');
+import axios from 'axios';
+import express = require('express');
+import { Response } from 'express';
+import webpack = require('webpack');
+import webpackDevMiddleware = require('webpack-dev-middleware');
+/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
+/* @ts-ignore */
+import webpackConfig = require('../webpack-config');
 
-// const app = express();
+const app = express();
 
-// const PORT = 3030;
+const PORT = 3030;
 
-// app.use(webpackDevMiddleware(webpack(webpackConfig)));
-// app.get('/app', (_, res: Response) => {
-//     res.sendFile(path.resolve(__dirname, 'index.html'));
-// });
+app.use(webpackDevMiddleware(webpack(webpackConfig)));
+app.get('/app', (_, res: Response) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
 
-// app.listen(PORT, () => 'Dev server started');
+app.get('/api*', (_, res: Response) => {
+    axios.get(`http://localhost:8080${_.path}`)
+        .then((response) => res.json(response.data))
+        .catch(e => console.error(e));
+});
 
-
-// const axios = require('axios');
-
-// axios.get('http://localhost:8080/api/ping')
-// // axios.get('api/ping')
-// //@ts-ignore
-//     .then((response) => {
-//         console.log(response.data.message)
-//     })
-//     //@ts-ignore
-//     .catch(function (error) {
-//         console.log(error)
-//     });
-
-// app.use(webpackDevMiddleware(webpack(webpackConfig)));
-// app.get('/app', (_, res: Response) => {
-    // res.sendFile(path.resolve(__dirname, 'index.html'));
-// });
-// app.get('/app', (_, res: Response) => {
-    // res.sendFile(path.resolve(__dirname, 'index.html'));
-// });
-
-
+app.listen(PORT, () => 'Dev server started');
