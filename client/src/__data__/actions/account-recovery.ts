3e6ba14803ee6e056443cf089cce9d1ce7c56ae3
sleonia <nikita.dontsov@yahoo.com>
2021-04-25 12:49:20 +0300

    Rolled back the changes

--- client/src/__data__/actions/account-recovery.ts
+++ client/src/__data__/actions/account-recovery.ts
@@ -1,9 +1,28 @@
-import { Feedback, State, Status } from '../types';
+import { Dispatch } from 'redux';
 
-export const status = (state: State): Status => {
-    return state.feedbacks.status;
-};
+import * as types from '../action-types';
+import { convertArgsToUrlParams } from '../utils/convert-args-to-url-params';
 
-export const data = (state: State): Feedback[] => {
-    return state.feedbacks.data;
+export default (password: string ) => {
+    return async (dispatch: Dispatch): Promise<void> => {
+        dispatch({
+            type: types.ACCOUNT_RECOVERY_LOADING
+        });
+
+        const url = new URL('http://localhost:3030/api/account-recovery');
+        const params = convertArgsToUrlParams({ password });
+        url.search = new URLSearchParams(params).toString();
+        const response = await fetch(url.toString());
+
+        if (!response.ok) {
+            console.warn(`An error has occured: ${response.status}`);
+            dispatch({
+                type: types.ACCOUNT_RECOVERY_ERROR
+            });
+        } else {
+            dispatch({
+                type: types.ACCOUNT_RECOVERY_FETCH
+            });
+        }
+    };
 };
