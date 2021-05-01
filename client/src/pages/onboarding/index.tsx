e00b9f929590d477578a847ba3acb5522b8745c0
sleonia <sleonia@student.21-school.ru>
2021-05-01 16:20:57 +0300

    Added template for main-component in /onboarding

--- client/src/pages/onboarding/index.tsx
+++ client/src/pages/onboarding/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import { Header } from '../../components/header';
+import { Main } from './components/main';
 import { Footer } from '../../components/footer';
 
 export const OnBoarding = (): JSX.Element => {
     return (
         <>
             <Header />
-            <main></main>
+            <Main />
             <Footer />
         </>
     );
