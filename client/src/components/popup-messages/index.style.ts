63a9e93b8028ccc0cf7b885d84276554fe49bacc
Донцов Никита Андреевич <NAndDontsov@sberbank.ru>
2021-08-21 17:03:57 +0300

    Removed useless shadow

--- client/src/components/popup-messages/index.style.ts
+++ client/src/components/popup-messages/index.style.ts
@@ -2,14 +2,9 @@ import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 import { Snackbar, SnackbarProps } from '@material-ui/core';
 
-import { LIGHT } from '../theme';
 import { themeTypeCast } from '../../utils';
 
-export const SnackbarStyled = styled(Snackbar)(({ theme, currentTheme }: StyledProps & SnackbarProps) => css`
-    ${currentTheme === LIGHT
-        ? 'box-shadow:  5px 5px 10px #e3e3e3, -5px -5px 10px #ffffff;'
-        : 'box-shadow:  5px 5px 10px #222222, -5px -5px 10px #2a2a2a;'
-}
+export const SnackbarStyled = styled(Snackbar)(({ theme }: StyledProps & SnackbarProps) => css`
     & > .MuiSnackbarContent-root {
         background-color: ${themeTypeCast(theme).primary};
         color: ${themeTypeCast(theme).secondary};
